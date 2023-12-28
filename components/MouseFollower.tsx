4110627b2e5aa8c15c5a83062c8f2fe5c70e407f
Yichong <yichong@reluvate.com>
2023-12-28 21:11:06 +0800

    [mouse] update dot following the cursor

--- components/MouseFollower.tsx
+++ components/MouseFollower.tsx
@@ -2,7 +2,6 @@ import { transcode } from "buffer";
 import React, { useState, useEffect, useRef } from "react";
 
 const MouseFollower: React.FC = () => {
-    const [scrollPercentage, setScrollPercentage] = useState<number>(0);
     const mouseFollowerRef = useRef<HTMLDivElement>(null);
     useEffect(() => {
         // Mouse move handler
@@ -13,45 +12,21 @@ const MouseFollower: React.FC = () => {
                 const keyframes = {
                     transform: `translate(${x}px, ${y}px)`,
                 };
-
-                mouseFollowerRef.current.animate(keyframes, { duration: 800 });
-            }
-        };
-
-        // Scroll handler
-        const handleScroll = () => {
-            const scrollPosition = window.scrollY;
-            const viewportHeight = window.innerHeight;
-            const newScrollValue = Math.min(scrollPosition / viewportHeight, 1);
-            console.log("newScrollValue: ", newScrollValue);
-            setScrollPercentage(newScrollValue);
-            if (mouseFollowerRef.current) {
-                mouseFollowerRef.current.style.opacity = `${newScrollValue}`;
+                mouseFollowerRef.current.animate(keyframes, {
+                    duration: 800,
+                    fill: "forwards",
+                });
             }
         };
 
         // Attach event listeners
         document.addEventListener("mousemove", handleMouseMove);
-        window.addEventListener("scroll", handleScroll);
 
         // Cleanup function
         return () => {
             document.removeEventListener("mousemove", handleMouseMove);
-            window.removeEventListener("scroll", handleScroll);
         };
     }, []);
-
-    const followerStyle: React.CSSProperties = {
-        position: "fixed",
-        pointerEvents: "none",
-        width: "100px",
-        height: "100px",
-        // left: `${mouseClientX}px`,
-        // top: `${mouseClientY}px`,
-        backgroundColor: `rgba(128, 0, 128, ${scrollPercentage / 100})`,
-        opacity: scrollPercentage > 0 ? 1 : 0,
-        transition: "opacity 0.3s",
-    };
 
     return (
         <>
