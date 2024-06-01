3247eea3ccb553f6a1629ff7596a4840f41bb2cf
YiChong <yichong@reluvate.com>
2024-06-01 14:05:28 +0800

    add in google analytic

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,23 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
     return (
         <Html>
-            <Head />
+            <Head>
+                {/* Google tag (gtag.js) */}
+                <script
+                    async
+                    src="https://www.googletagmanager.com/gtag/js?id=G-YT8N03KS5S"
+                ></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-YT8N03KS5S');
+                    `,
+                    }}
+                />
+            </Head>
             <body>
                 <Main />
                 <NextScript />
